a7254a81a8bf525dc2669edb5eb2d3ae52b737d0
OlamidotunIY <olamidotun225@gmail.com>
2026-06-08 23:15:26 +0100

    refactor(layout): improve code formatting and remove unused styles

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,33 +1,30 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
-import {
-  ThemeProvider,
-  useTheme,
-} from '@/components/ui/ThemeProvider/ThemeProvider';
+import
+  {
+    ThemeProvider,
+    useTheme,
+  } from '@/components/ui/ThemeProvider/ThemeProvider';
 import '@/global.css';
 
-function RootStack() {
+function RootStack()
+{
   const { resolvedTheme } = useTheme();
   const isDark = resolvedTheme === 'dark';
-  const backgroundColor = isDark ? '#121212' : '#ffffff';
-  const foregroundColor = isDark ? '#fefeff' : '#171717';
 
   return (
     <>
       <StatusBar style={isDark ? 'light' : 'dark'} />
-      <Stack
-        screenOptions={{
-          contentStyle: { backgroundColor },
-          headerStyle: { backgroundColor },
-          headerTintColor: foregroundColor,
-        }}
-      />
+      <Stack screenOptions={{
+        headerShown: false
+      }} />
     </>
   );
 }
 
-export default function RootLayout() {
+export default function RootLayout()
+{
   return (
     <ThemeProvider>
       <RootStack />
